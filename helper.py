a0c9d52781a0dc8f5a0c4a2093c1624deb60b4c6
Amit Gupta <amitgupta2533@gmail.com>
2020-05-29 10:37:17 +0530

    Added helper file

---
+++ helper.py
@@ -0,0 +1,14 @@
+def add():
+    pass
+    
+def subtract():
+    #So I have added the subtract function to check if it shows in diffs
+    #So added second line
+    #So added third line
+    pass
+    
+def multiply():
+    pass
+    
+def divide():
+    pass
