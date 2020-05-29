3db27d7a91356eb74e09cf3e7db1257a551c7606
Amit Gupta <amitgupta2533@gmail.com>
2020-05-29 11:02:38 +0530

    Completed Subtract Function

--- helper.py
+++ helper.py
@@ -5,7 +5,8 @@ def subtract():
     #So I have added the subtract function to check if it shows in diffs
     #So added second line
     #So added third line
-    pass
+    return x - y
+    
     
 def multiply():
     pass
